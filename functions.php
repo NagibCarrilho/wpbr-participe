c25871ad77ae6637c7e2ee801d8722fff344a845
Vinicius Massuchetto <vmassuchetto@gmail.com>
2014-04-21 09:42:46 -0300

    Tries to load only relevant users for mentions
    refs #1

--- functions.php
+++ functions.php
@@ -1,11 +1,10 @@
 <?php
-ini_set('log_errors', '1');
-ini_set('error_log', '/home/wordpressbr/wp-brasil.org/error.log');
+
+require_once( get_template_directory() . '/inc/ajax.php' );
+require_once( get_template_directory() . '/inc/terms-in-comments.php' );
+require_once( get_template_directory() . '/inc/mentions.php' );
 
 add_action( 'wp_enqueue_scripts', 'participe_styles' );
-require_once( get_template_directory() . '/inc/ajax.php' );
-
-
 function participe_styles() {
 
 	wp_register_style( 'p2', get_template_directory_uri().'/style.css' );
@@ -16,4 +15,44 @@ function participe_styles() {
 
 }
 
-?>
+class Participe_P2_Mentions extends P2_Mentions {
+
+    function load_users() {
+
+        // Cache the user information.
+		if ( ! empty( $this->users ) )
+	 		return $this->users;
+
+        global $wp_query;
+
+        $users = array();
+
+        // Get the last active users only for the queried posts
+        foreach( $wp_query->posts as $p ) {
+            $users[] = $p->post_author;
+            $args = array(
+                'post_id' => $p->ID,
+                'number' => 10
+            );
+            foreach( get_comments( $args ) as $c ) {
+                $users[] = $c->user_id;
+            }
+        }
+
+        foreach( $users as $user ) {
+            if ( $u = get_user_by( 'id', $user ) ) {
+                $this->users[ $u->data->ID ] = $u->data;
+                $this->names[ $u->data->ID ] = $u->user_nicename;
+            }
+        }
+
+		return $this->users;
+
+    }
+
+}
+
+add_filter( 'p2_add_component_mentions', 'participe_add_component_mentions' );
+function participe_add_component_mentions() {
+    return 'Participe_P2_Mentions';
+}
